d80f2b4b9d6ec5d5216a9d0616d1d6ec14333ed9
Safari Germain <99614251+germainsafari@users.noreply.github.com>
2023-11-26 22:43:02 +0100

    Update nurses.py

--- nurses.py
+++ nurses.py
@@ -3,4 +3,5 @@ class Nurse(User):
     def __init__(self, name, surname, pesel):
         super().__init__(name, surname, pesel)
 
-
+    def add_patient(self, patient):
+        self.patients.append(patient)
